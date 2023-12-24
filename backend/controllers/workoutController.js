650cc4de2b30a93bbb3b837ff0dc987fc94a8cf2
inspectNad <baldonadodante5@gmail.com>
2023-12-24 19:19:04 +0800

    added modules controllers and routes including model

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -31,6 +31,20 @@ const getWorkout = async (req, res) => {
 const createWorkout = async (req, res) => {
   const {title, load, reps} = req.body
 
+  let emptyFields = []
+
+  if(!title) {
+    emptyFields.push('title')
+  }
+  if(!load) {
+    emptyFields.push('load')
+  }
+  if(!reps) {
+    emptyFields.push('reps')
+  }
+  if(emptyFields.length > 0) {
+    return res.status(404).json({error: 'Please fill in all the fields', emptyFields})
+  }
   // Add doc to db
   try{
     const workout = await Workout.create({ title, load, reps})
@@ -59,9 +73,28 @@ const deleteWorkout = async (req, res) => {
 
 
 // Update a workout
+const updateWorkout = async (req, res) => {
+  const { id } = req.params
+
+  if(!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({error: 'No such workout'})
+  }
+
+  const workout = await Workout.findOneAndUpdate({_id: id}, {
+    ...req.body
+  })
+
+  if(!workout){
+    return res.status(404).json({error: 'No such workout'})
+  }
+
+  res.status(200).json(workout)
+}
 
 module.exports = {
   getWorkouts,
   getWorkout,
-  createWorkout
+  createWorkout,
+  deleteWorkout,
+  updateWorkout
 }
--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -2,7 +2,9 @@ const express = require('express')
 const {
   createWorkout,
   getWorkout,
-  getWorkouts
+  getWorkouts,
+  deleteWorkout,
+  updateWorkout
 } = require('../controllers/workoutController')
 
 const router = express.Router()
@@ -17,13 +19,9 @@ router.get('/:id', getWorkout)
 router.post('/', createWorkout)
 
 // Delete a workout
-router.delete('/:id', (req, res ) => {
-  res.json({mssg: 'Delete a workout'})
-})
+router.delete('/:id', deleteWorkout)
 
-router.patch('/:id', (req, res ) => {
-  res.json({mssg: 'Update new workout'})
-})
+router.patch('/:id', updateWorkout)
 
 
 module.exports = router
